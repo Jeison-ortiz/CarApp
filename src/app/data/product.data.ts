1239e603dd7f3d99de8aed2cf7704c8ca88cda7e
Jeison Ortiz <ortizjeison4@gmail.com>
2024-05-18 10:57:11 -0500

    feat: Se crea la data ficticia para maquetar la data.

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[]=[
+  {
+    id:1,
+    name: 'Teclado Asus',
+    description: 'Teclado con luces RGB',
+    price: 1000
+  },
+  {
+    id:2,
+    name: 'Televisor',
+    description: 'Samsun Smart TV',
+    price: 1200
+  },
+  {
+    id:3,
+    name: 'Mouse inteligente',
+    description: 'Mouse RGB dependiendo de la claridad de luz del día, con veneficios relajantes para tus eyes',
+    price: 3000
+  },
+  {
+    id:4,
+    name: 'Camara',
+    description: 'Camara Sony con 10000px y con transmisor de fibra optica imcorporado',
+    price: 4000
+  },
+  {
+    id:5,
+    name: 'Celular Iphone',
+    description: 'Celular Iphone 19, ultima versión 2026 con inteligencia artificial para preparar la cena',
+    price: 5000
+  },
+  {
+    id:6,
+    name: 'Computado lenovo',
+    description: 'Computador lenovo con pantalla tactil y bateria de larga duración',
+    price: 6000
+  }
+]
